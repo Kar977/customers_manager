fade53957ab55bf9f129d99198a19e0d84d53b87
Kar977 <karolmatuszak7@gmail.com>
2025-02-25 18:07:05 +0100

    [REFACTOR] changed db connection to async and adjusted queries in functions

--- customers_manager/customers/routers.py
+++ customers_manager/customers/routers.py
@@ -1,7 +1,6 @@
 from customers.schemas import (
     CreateVisitRequest,
     DeleteCustomerRequest,
-    SetSlotAvailableRequest,
     SetSlotStatusRequest,
     CreateWorkdayRequest,
     DeleteWorkdayRequest,
@@ -13,6 +12,7 @@ from customers.services.crud import (
     visitation_manager_obj,
 )
 from database_structure.database import get_db, SessionLocal
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Depends, Request
 from fastapi.responses import JSONResponse
 
@@ -21,18 +21,18 @@ router = APIRouter(prefix="/customers")
 
 @router.get("/slots/available/")
 async def view_all_open_dates_with_at_least_one_slot_available(
-    request: Request, db: SessionLocal = Depends(get_db)
+    request: Request, db: AsyncSession = Depends(get_db)
 ):
 
-    return visitation_manager_obj.get_all_available_slots(db)
+    return await visitation_manager_obj.get_all_available_slots(db)
 
 
 @router.get("/slots/available/{slot_date}")
 async def view_free_slots_on_specific_day(
-    request: Request, slot_date: str, db: SessionLocal = Depends(get_db)
+    request: Request, slot_date: str, db: AsyncSession = Depends(get_db)
 ):
 
-    available_slots = visitation_manager_obj.read_all_available_hours_on_specific_date(
+    available_slots = await visitation_manager_obj.read_all_available_hours_on_specific_date(
         slot_date, db
     )
     return JSONResponse({slot_date: available_slots})
@@ -40,10 +40,10 @@ async def view_free_slots_on_specific_day(
 
 @router.post("/visit/", status_code=201)
 async def create_visit(
-    new_visit: CreateVisitRequest, db: SessionLocal = Depends(get_db)
+    new_visit: CreateVisitRequest, db: AsyncSession = Depends(get_db)
 ):
 
-    visit_reservation = visitation_manager_obj.reserve_visitation(
+    visit_reservation = await visitation_manager_obj.reserve_visitation(
         new_visit.name, new_visit.phone_nbr, new_visit.date, new_visit.slot, db
     )
 
@@ -52,47 +52,47 @@ async def create_visit(
 
 @router.delete("/customer/", status_code=200)
 async def delete_customer(
-    customer_request: DeleteCustomerRequest, db: SessionLocal = Depends(get_db)
+    customer_request: DeleteCustomerRequest, db: AsyncSession = Depends(get_db)
 ):
 
-    return customers_manager_obj.delete_customer_and_release_slots(
+    return await customers_manager_obj.delete_customer_and_release_slots(
         id_nbr=customer_request.user_id, db=db
     )
 
 
 @router.put("/slot/status/")
 async def set_slot_status(
-    slot_request: SetSlotStatusRequest, db: SessionLocal = Depends(get_db)
+    slot_request: SetSlotStatusRequest, db: AsyncSession = Depends(get_db)
 ):
 
-    return workday_manager_obj.put_new_slot_status(
+    return await workday_manager_obj.put_new_slot_status(
         slot_id=slot_request.slot_id, slot_status=slot_request.slot_status, db=db
     )
 
 
 @router.delete("/slot/")
 async def delete_slot(
-    slot_request: DeleteSlotRequest, db: SessionLocal = Depends(get_db)
+    slot_request: DeleteSlotRequest, db: AsyncSession = Depends(get_db)
 ):
 
-    return workday_manager_obj.delete_slot(slot_id=slot_request.slot_id, db=db)
+    return await workday_manager_obj.delete_slot(slot_id=slot_request.slot_id, db=db)
 
 
 @router.post("/workday/", status_code=201)
 async def create_workday(
-    workday_request: CreateWorkdayRequest, db: SessionLocal = Depends(get_db)
+    workday_request: CreateWorkdayRequest, db: AsyncSession = Depends(get_db)
 ):
 
-    return workday_manager_obj.create_workday(
+    return await workday_manager_obj.create_workday(
         date=workday_request.date, day_status=workday_request.day_status, db=db
     )
 
 
 @router.delete("/workday/", status_code=204)
 async def delete_workday(
-    workday_request: DeleteWorkdayRequest, db: SessionLocal = Depends(get_db)
+    workday_request: DeleteWorkdayRequest, db: AsyncSession = Depends(get_db)
 ):
 
-    return workday_manager_obj.delete_workday(
+    return await workday_manager_obj.delete_workday(
         workday_id=workday_request.workday_id, db=db
     )
