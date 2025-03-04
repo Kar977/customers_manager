0684277f870e5bac6f94527b33561cd3c6fc6639
Kar977 <karolmatuszak7@gmail.com>
2025-03-04 19:06:15 +0100

    [REFACTOR] added async url with connection to db into file with settings

--- customers_manager/settings.py
+++ customers_manager/settings.py
@@ -1,4 +1,4 @@
-from pydantic import Field
+from pydantic import Field, model_validator
 from pydantic_settings import BaseSettings
 
 
@@ -8,6 +8,12 @@ class Settings(BaseSettings):
     postgres_host: str = Field(default="customer-db")
     postgres_port: str = Field(default="5432")
     postgres_name: str = Field(default="postgres_db")
+    ASYNC_DATABASE_URL = ""
 
+    @model_validator(mode='after')
+    def db_url(self):
+        self.ASYNC_DATABASE_URL = (
+            f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_name}"
+        )
 
 settings = Settings()
